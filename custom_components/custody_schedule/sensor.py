b09278729883b5566b637726930f6720cd88574f
Jack Ngl <jackn@hotmail.com>
2026-02-04 13:20:50 +0100

    style: combined imports in homeassistant.util for isort and release 1.8.28

--- custom_components/custody_schedule/sensor.py
+++ custom_components/custody_schedule/sensor.py
@@ -17,8 +17,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.util import dt as dt_util
-from homeassistant.util import slugify
+from homeassistant.util import dt as dt_util, slugify
 
 from . import CustodyScheduleCoordinator
 from .const import (
